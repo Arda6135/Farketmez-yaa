d8af65534f0a93c3b82c0d37313c39d74e109e5a
ArdaKaracal <arda.karacal@moralabs.com>
2026-06-25 22:38:56 +0300

    main edit for test optimization

--- main.py
+++ main.py
@@ -7,37 +7,52 @@ from KNN import PyTorchKNNClassifier
 def main():
     
     #Optimnization
-    F_max_features = 2000
-    P_components = 50
-    K_neighbors = 5
+    F_values = [100,200]
+    P_values = [5,10]
+    K_values = [5,10]
 
     # Split dataset
     splitter = DataSplitter()
     x_train, x_val, x_test, y_train, y_val, y_test = splitter.splitData()
-    print(f"Train: {len(x_train)}, Val: {len(x_val)}, Test: {len(x_test)}")
+    
+    best_mse = float('inf')
+    best_accuracy = 0.0
+    best_f1 = 0.0
+    
+    OutputTxt = "Results.txt"
+    with open(OutputTxt, "w") as f:
+        f.write(f"Train: {len(x_train)}, Val: {len(x_val)}, Test: {len(x_test)}\n")
+
     # Vectorize text data using TF-IDF
-    Extractor = FeatureExtractor(max_features=F_max_features)
-    x_train_extracted = Extractor.fit_transform_train(x_train)
-    x_val_extracted = Extractor.transform_unseen(x_val)
+    for F_max_features in F_values:
+        Extractor = FeatureExtractor(max_features=F_max_features)
+        x_train_extracted = Extractor.fit_transform_train(x_train)
+        x_val_extracted = Extractor.transform_unseen(x_val)
+        for P_components in P_values:
+            # Apply PCA for dimensionality reduction
+            if(P_components > F_max_features):
+                continue
+            transformer = PcaTransformer(n_components=P_components)
+            x_train_pca = transformer.fit_transform_train(x_train_extracted)
+            x_val_pca = transformer.transform_unseen(x_val_extracted)
+            for K_neighbors in K_values:
+                # Initialize KNN
+                knn = PyTorchKNNClassifier(k=K_neighbors)
+                knn.fit(x_train_pca, y_train)
+                # Evaluate on validation set(Model Gecerleme)
+                mse, accuracy, f1 = knn.evaluate(x_val_pca, y_val)
 
-    # Apply PCA for dimensionality reduction
-    transformer = PcaTransformer(n_components=P_components)
-    x_train_pca = transformer.fit_transform_train(x_train_extracted)
-    x_val_pca = transformer.transform_unseen(x_val_extracted)
-
-    # Initialize KNN
-    knn = PyTorchKNNClassifier(k=K_neighbors)
-    knn.fit(x_train_pca, y_train)
-
-    # Evaluate on validation set(Model Gecerleme)
-    mse, accuracy, f1 = knn.evaluate(x_val_pca, y_val)
-
-    print("Feature Vector Dimension (F) : ", F_max_features)
-    print("PCA Output Vector Dim (P)    : ", P_components)
-    print("KNN Neighbors (K)            : ", K_neighbors)
-    print("Mean Squared Error (MSE)     : ", mse)
-    print("Accuracy   : ", accuracy)
-    print("F1-Score   : ", f1)
-
+                if mse < best_mse:
+                    best_mse = mse
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                if f1 > best_f1:
+                    best_f1 = f1
+                with open(OutputTxt, "a") as f:
+                    f.write(f"F: {F_max_features}, P: {P_components}, K: {K_neighbors}\n")
+                    f.write(f"MSE: {mse}, Accuracy: {accuracy}, F1: {f1}\n")
+                    f.write("--------------------------------------------------\n")
+    with open(OutputTxt, "a") as f:
+        f.write(f"Lowest MSE: {best_mse}, Highest Accuracy: {best_accuracy}, Highest F1: {best_f1}\n")
 if __name__ == "__main__":
     main()
